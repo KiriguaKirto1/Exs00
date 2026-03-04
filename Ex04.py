71487ec0f04a42effd29f688f61b75921b29c07b
Gustavo Chapaval Alves <140844146+KiriguaKirto1@users.noreply.github.com>
2026-03-04 20:39:18 -0300

    Fix age input type in Ex04.py

--- Ex04.py
+++ Ex04.py
@@ -1,7 +1,8 @@
 #Quarto repositório DS1-sub Aula 1
 
 Name = input("Digite seu nome: ")
-Idade = int(input("Digite sua idade: "))
+Idade = input("Digite sua idade: ")
 
 
 print(f'Olá {Name}, seja bem vindo! Você tem {Idade} anos.')
+
